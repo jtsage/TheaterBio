02f653bfe239f231077405b6096c6c5249e4b728
JTSage <jtsage+datebox@gmail.com>
2019-06-22 19:11:22 -0400

    Create migrations

---
+++ config/Migrations/20190622231031_Initial.php
@@ -0,0 +1,298 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+    public function up()
+    {
+
+        $this->table('bios', ['id' => false, 'primary_key' => ['id']])
+            ->addColumn('id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('user_id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('purpose_id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('text', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('updated_at', 'timestamp', [
+                'default' => '1970-01-01 05:00:01',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created_at', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'user_id',
+                    'purpose_id',
+                ],
+                ['unique' => true]
+            )
+            ->addIndex(
+                [
+                    'purpose_id',
+                ]
+            )
+            ->addIndex(
+                [
+                    'user_id',
+                ]
+            )
+            ->create();
+
+        $this->table('headshots', ['id' => false, 'primary_key' => ['id']])
+            ->addColumn('id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('user_id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('purpose_id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('file', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('dir', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('updated_at', 'timestamp', [
+                'default' => '1970-01-01 05:00:01',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created_at', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'purpose_id',
+                    'user_id',
+                ],
+                ['unique' => true]
+            )
+            ->addIndex(
+                [
+                    'purpose_id',
+                ]
+            )
+            ->addIndex(
+                [
+                    'user_id',
+                ]
+            )
+            ->create();
+
+        $this->table('purposes', ['id' => false, 'primary_key' => ['id']])
+            ->addColumn('id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => false,
+            ])
+            ->addColumn('description', 'string', [
+                'default' => null,
+                'limit' => 150,
+                'null' => true,
+            ])
+            ->addColumn('is_active', 'boolean', [
+                'default' => true,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->create();
+
+        $this->table('users', ['id' => false, 'primary_key' => ['id']])
+            ->addColumn('id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('username', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('first', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => false,
+            ])
+            ->addColumn('last', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => false,
+            ])
+            ->addColumn('print_name', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => true,
+            ])
+            ->addColumn('is_active', 'boolean', [
+                'default' => true,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('is_password_expired', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('is_admin', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('is_verified', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('last_login_at', 'timestamp', [
+                'default' => '1970-01-01 05:00:01',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created_at', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('updated_at', 'timestamp', [
+                'default' => '1970-01-01 05:00:01',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('reset_hash', 'string', [
+                'default' => null,
+                'limit' => 60,
+                'null' => true,
+            ])
+            ->addColumn('reset_hash_time', 'timestamp', [
+                'default' => '1970-01-01 05:00:01',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('verify_hash', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'reset_hash',
+                ],
+                ['unique' => true]
+            )
+            ->addIndex(
+                [
+                    'username',
+                ],
+                ['unique' => true]
+            )
+            ->create();
+
+        $this->table('bios')
+            ->addForeignKey(
+                'purpose_id',
+                'purposes',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'CASCADE'
+                ]
+            )
+            ->addForeignKey(
+                'user_id',
+                'users',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'CASCADE'
+                ]
+            )
+            ->update();
+
+        $this->table('headshots')
+            ->addForeignKey(
+                'purpose_id',
+                'purposes',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'CASCADE'
+                ]
+            )
+            ->addForeignKey(
+                'user_id',
+                'users',
+                'id',
+                [
+                    'update' => 'RESTRICT',
+                    'delete' => 'CASCADE'
+                ]
+            )
+            ->update();
+    }
+
+    public function down()
+    {
+        $this->table('bios')
+            ->dropForeignKey(
+                'purpose_id'
+            )
+            ->dropForeignKey(
+                'user_id'
+            )->save();
+
+        $this->table('headshots')
+            ->dropForeignKey(
+                'purpose_id'
+            )
+            ->dropForeignKey(
+                'user_id'
+            )->save();
+
+        $this->table('bios')->drop()->save();
+        $this->table('headshots')->drop()->save();
+        $this->table('purposes')->drop()->save();
+        $this->table('users')->drop()->save();
+    }
+}
